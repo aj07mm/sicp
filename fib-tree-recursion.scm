e03132ef3afee5b58932cd5dea9103d16abb926f
aj07mm <aj07mm@hotmail.com>
2019-10-01 16:45:58 -0300

    fib

--- fib-tree-recursion.scm
+++ fib-tree-recursion.scm
@@ -3,4 +3,10 @@
 		  ((= n 1) 1)
 		  (else (+ (fib (- n 1))
 		           (fib (- n 2))))))
+
+(define (fib n ) ; linear iterative process
+	(if (= count 0)
+		b
+		(fib-iter (+ a b) a (- count 1))))
+
 (fib 5)
